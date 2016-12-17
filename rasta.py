e7a22d4d90ead9871151bbaf8dee35d8e4348140
Anthony Liu <anthonyscottliu@gmail.com>
2016-12-17 02:43:53 -0500

    Develops a utility function to generate box models.

--- rasta.py
+++ rasta.py
@@ -9,6 +9,7 @@ def render(camera, model):
     data.fill(255)
     img = Image.fromarray(data, 'RGB')
     draw = ImageDraw.Draw(img, 'RGB')
+    # print model
     for triangle, color in model['triangles']:
         points = filter(
             lambda x: x is not None,
@@ -23,7 +24,7 @@ def render(camera, model):
 
 def point_to_pixel(raw_p):
     p = np.array(raw_p + [1])
-    cam_space = get_camera([0, 0., -15.], [0, 0, -1])
+    cam_space = get_camera([10, 10, 10.], [0, 0, -1])
 
     near_plane_dist = 5.
     near_plane_width = 160.
@@ -49,6 +50,7 @@ def point_to_pixel(raw_p):
 
 
 def get_camera(pos, z_axis):
+    pos = [-a for a in pos]
     z_axis = z_axis / np.linalg.norm(np.array(z_axis))
     z_axis = np.append(z_axis, 0)
 
@@ -82,8 +84,6 @@ def get_camera(pos, z_axis):
         np.array(z_axis),
         np.array(pos + [1])
     ])
-    print y_rot, x_rot
-    print cam_mat
     return np.linalg.inv(cam_mat)
 
 
@@ -141,10 +141,53 @@ def parse_point_list(points):
         return None
 
 
+def get_box(pos, l, w, h, color=False):
+    triangles = []
+    points = [
+        [i >> 2, (i >> 1) & 1, i & 1] for i in range(2**3)
+    ]
+    points = [
+        [l*a + pos[0], w*b + pos[1], h*c + pos[2]]
+        for a, b, c in points
+    ]
+    faces = [
+        [0, 1, 3, 2],  # x-axis
+        [4, 5, 7, 6],  # x-axis
+        [0, 1, 5, 4],  # y-axis
+        [2, 3, 7, 6],  # y-axis
+        [0, 2, 6, 4],  # z-axis
+        [1, 3, 7, 5]  # z-axis
+    ]
+    m = 20
+
+    for face in faces:
+        base_color = tuple([
+            max(min(int(np.random.uniform() * 256), 255), 0)
+            for _ in range(3)
+        ])
+        if not (color is False):
+            base_color = color
+        color1 = tuple([
+            max(min(int(a + m), 255), 0)
+            for a in base_color
+        ])
+        color2 = tuple([
+            max(min(int(a - m), 255), 0)
+            for a in base_color
+        ])
+
+        pa, pb, pc, pd = [points[x] for x in face]
+        triangles.append(([pa, pb, pc], color1))
+        triangles.append(([pa, pc, pd], color2))
+
+    return {'triangles': triangles}
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         sys.exit('Usage: %s CAMERA_FILE_NAME MODEL_FILE_NAME' % sys.argv[0])
 
     camera = parse_camera_file(sys.argv[1])
     model = parse_graphics_file(sys.argv[2])
+    model = get_box([0, 0, 0], 40, 40, 40)
     render(camera, model)
