28207d525ed49a32aa77cd1e3255e0c076b956c0
Anthony Liu <anthonyscottliu@gmail.com>
2016-12-18 04:05:26 -0500

    Packages the Rasta renderer into a class.

---
+++ tests/test_renderer.py
@@ -0,0 +1,23 @@
+from context import Rasta
+
+camera = '../data/camera.cam'
+out_file = '../out.png'
+global_offset = [-84., -62., 0.]
+global_scale = [12.6, 12.6, 10.]
+rasta = Rasta(camera, global_offset, global_scale, 800, 600)
+
+boundary = Rasta.get_box(
+    [0, 0, 0],
+    13.3333, 10, 10,
+    color=(0, 0, 0),
+    outline=True
+)
+models = [boundary]
+models.append(Rasta.get_box(
+    [0, 0, 0],
+    1, 1, 1,
+    color=(255, 0, 0),
+    outline=False
+))
+
+rasta.render(models, out_file)
